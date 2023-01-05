3f3f78267c70bc607b7806af46214ed2ed799ad5
Anastasia Shcheblykina <anastasia23121993@gmail.com>
2023-01-05 23:18:35 +0300

    create project

--- src/test/java/ru/netology/Radio/RadioTest.java
+++ src/test/java/ru/netology/Radio/RadioTest.java
@@ -5,5 +5,18 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
     @Test
+    public void NextStationNumber() {
+        Radio radio = new Radio(8);
+
+        radio.setStationNumber(8);
+
+        int expected = 8;
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected,actual);
+
+    }
+
+}
 
 
